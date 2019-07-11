fb383afe28ea80157774ea516ae01112c4abdabd
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2019-07-11 22:11:32 +0530

    Add files via upload

---
+++ Get.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Get
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		String text= scan.nextLine();
+		System.out.println(text);
+		int num = scan.nextInt();
+		System.out.println(num);
+	}
+}
