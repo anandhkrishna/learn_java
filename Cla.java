676cc8320799fae7d41ef4a78d80935c127183a2
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2019-07-14 09:31:04 +0530

    Add files via upload

---
+++ Cla.java
@@ -0,0 +1,13 @@
+class Cla
+{
+	public static void main(String args[])
+	{
+		if(args.length==2)
+		{
+			int a=Integer.parseInt(args[0]);
+			int b=Integer.parseInt(args[1]);
+			int c = a+b;
+			System.out.println(c);
+		}
+	}
+}
